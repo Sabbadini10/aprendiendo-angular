8dc2c3a495660de5bef6d0475dcc42e0a57e6b4f
Sabbadini10 <matiassabbadini24@gmail.com>
2023-02-06 16:12:20 -0300

    termine maquetado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+/*   tamano=30;
+agrandarAchicar= {
+  "width.em": 70,
+ "margin-left.em": 15 } */
 
+
+/* reducir() {
+  this.tamano--;
+  this.agrandarAchicar["width.em"]=this.tamano;
+  this.agrandarAchicar["margin-left.em"]=this.tamano; 
+} */
 }
