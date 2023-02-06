d34fb1d2645ef16259c341236e213ff9d8c89076
Sabbadini10 <matiassabbadini24@gmail.com>
2023-02-05 21:50:22 -0300

    cree componentes y termine primera parte del maquetado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ItemsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
